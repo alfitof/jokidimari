71722629b5dcae3015f59b5f267b818c6c140eff
alfitof <alfito.fbriansyah@gmail.com>
2024-09-04 03:17:34 +0700

    lanjut project

---
+++ data.js
@@ -0,0 +1,72 @@
+import {
+  IconHomeFilled,
+  IconInfoCircleFilled,
+  IconSettingsFilled,
+  IconBriefcaseFilled,
+} from "@tabler/icons-react";
+
+const products = [
+  {
+    title: "Product 1",
+    thumbnail:
+      "https://www.expgames.gg/explore/wp-content/uploads/2023/02/sakura-wishes-guinevere.webp",
+    link: "/product-1",
+  },
+  {
+    title: "Product 2",
+    thumbnail:
+      "https://www.expgames.gg/explore/wp-content/uploads/2023/02/sakura-wishes-guinevere.webp",
+    link: "/product-2",
+  },
+  {
+    title: "Product 3",
+    thumbnail:
+      "https://www.expgames.gg/explore/wp-content/uploads/2023/02/sakura-wishes-guinevere.webp",
+    link: "/product-3",
+  },
+  {
+    title: "Product 1",
+    thumbnail:
+      "https://www.expgames.gg/explore/wp-content/uploads/2023/02/sakura-wishes-guinevere.webp",
+    link: "/product-1",
+  },
+  {
+    title: "Product 2",
+    thumbnail:
+      "https://www.expgames.gg/explore/wp-content/uploads/2023/02/sakura-wishes-guinevere.webp",
+    link: "/product-2",
+  },
+  {
+    title: "Product 3",
+    thumbnail:
+      "https://www.expgames.gg/explore/wp-content/uploads/2023/02/sakura-wishes-guinevere.webp",
+    link: "/product-3",
+  },
+  {
+    title: "Product 1",
+    thumbnail:
+      "https://www.expgames.gg/explore/wp-content/uploads/2023/02/sakura-wishes-guinevere.webp",
+    link: "/product-1",
+  },
+  {
+    title: "Product 2",
+    thumbnail:
+      "https://www.expgames.gg/explore/wp-content/uploads/2023/02/sakura-wishes-guinevere.webp",
+    link: "/product-2",
+  },
+  {
+    title: "Product 3",
+    thumbnail:
+      "https://www.expgames.gg/explore/wp-content/uploads/2023/02/sakura-wishes-guinevere.webp",
+    link: "/product-3",
+  },
+];
+
+const navItems = [
+  { name: "Home", link: "/", icon: <IconHomeFilled /> },
+  { name: "About", link: "/about", icon: <IconInfoCircleFilled /> },
+  { name: "Services", link: "/services", icon: <IconSettingsFilled /> },
+  { name: "Portfolio", link: "/portfolio", icon: <IconBriefcaseFilled /> },
+];
+
+export { products, navItems };
